85b12941aa32c10e251b11612bb1763c4bf06be1
Sébastien JARDE <s.jarde@auvernge-rhone-alpes.cci.fr>
2025-06-10 11:10:48 +0200

    Add client connection

--- src/client/modules/store.js
+++ src/client/modules/store.js
@@ -2,5 +2,12 @@ const { reactive } = Vue
 
 const store = reactive(
     {
+        async StartNetworkClient()
+        {
+            this.colyseus = new Colyseus.Client("ws://localhost:2567")
+            this.room = await this.colyseus.joinOrCreate("game_room");
+        }
     }
 )
+
+store.StartNetworkClient()
