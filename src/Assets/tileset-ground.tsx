750b03e0e8b69a6543fbf179d841d5983b4babae
BartKnucle <sjarde@yahoo.fr>
2025-06-22 11:14:46 +0200

    Update tiles

--- src/Assets/tileset-ground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset-ground" tilewidth="128" tileheight="64" tilecount="3" columns="3">
- <image source="../client/assets/tileset-ground.png" width="384" height="64"/>
-</tileset>
--- src/Assets/tileset-rocks.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset-rocks" tilewidth="128" tileheight="128" tilecount="3" columns="3">
- <editorsettings>
-  <export target="../client/assets/map.json" format="json"/>
- </editorsettings>
- <image source="../client/assets/tileset-rocks.png" width="384" height="128"/>
-</tileset>
